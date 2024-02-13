0507dd9045469b5df66687f90db87bf953f02c48
carlsondultra <carlsondultra@users.noreply.github.com>
2024-02-12 21:13:15 -0500

    adding another test to the component

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,11 +1,23 @@
 import { render, screen } from '@testing-library/react'
 import Home from '@/app/page'
 
-//Basic test to check if Home component has the word "Docs" inside of it
-it('should have Docs text', () => {
-    render(<Home />) // ARRANGE 
 
-    const myElem = screen.getByText('Docs') // ACTION TAKEN
+describe('Home', () => {
+    //Basic test to check if Home component has the word "Docs" inside of it
+    it('should have Docs text', () => {
+        render(<Home />) // ARRANGE 
 
-    expect(myElem).toBeInTheDocument() //ASSERT
+        const myElem = screen.getByText('Docs') // ACTION TAKEN
+
+        expect(myElem).toBeInTheDocument() //ASSERT
+    })
+
+    //Test to check if Home component has the word "information" inside of it, using regex
+    it('should contain the text "information"', () => {
+        render(<Home />)
+
+        const myElem = screen.getByText(/information/i) //case insensitive
+
+        expect(myElem).toBeInTheDocument()
+    })
 })
